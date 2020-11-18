5f208ceff24b45a624323af7374fbe52b9772fff
benni <benedikt.witteler@tum.de>
2020-11-18 21:32:14 +0100

    Implementing number programm for coding challenge II

--- scr/main.c
+++ scr/main.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
+#include<espl_lib.h>
 
 int main(int argc, char* argv[]) {
     printf("Hello ESPL");
-    printf("This is a new print");
+    
+    unsigned int inputNumber;
+    char enterAnotherNumber = 'y';
+
+    do {
+        printf("\nPlease enter a number to convert into a word: ");
+        scanf("%u", &inputNumber);
+        getchar(); //scanf() leafes a \n in the input buffer. getchar() cleans it up
+        printf("%s\n", num_to_words(inputNumber));
+
+        printf("Do you want to enter another number? (y/n) ");
+        scanf("%c", &enterAnotherNumber);
+        getchar();
+    } while(enterAnotherNumber == 'y');
 }
